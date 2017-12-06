9a84c2d27690fbcb5e641c947ddbf5c158755b8f
DouglasReis <douglas.reis@totaljobsgroup.com>
2017-12-06 13:31:41 +0000

    Initial commit

---
+++ src/GdaxClient.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GdaxClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
